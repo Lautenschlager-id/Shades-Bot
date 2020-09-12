d5eb8a8c9b0a9972f635d62a480fcada6dfb0b6a
Lautenschlager <tai.romani@hotmail.com>
2020-09-12 15:14:10 -0300

    Add files via upload

--- data/lang/en.lua
+++ data/lang/en.lua
@@ -17,15 +17,16 @@ return {
 	nocmd = "Command '%s' not found. :s", -- Name
 	hlist = "Type '%s command_name' or '%scommand_name ?' to learn more. Available Commands → %s", -- "help"
 	title = "Checks the next titles you or someone is about to unlock. Accepts a nickname as parameter.",
-	hrank = "Displays the position of a player in Club-Mice's leaderboard. Accepts a nickname as parameter.",
+	hrank = "Displays the position of a player in CheeseForMice's leaderboard. Accepts a nickname as parameter.",
 	inv = "Sends the invitation link to join the 'Fifty Shades of Lua' discord server.",
 	rewards = "Generates a code to be used in [fiftysol.github.io/rewards], where you can see all rewards a player still can obtain in game. Accepts a nickname as parameter.",
 	mapcrew = "Displays the list of online Mapcrew and their respective review categories.",
+	hbgcolor = "Sends the background color of Transformice.",
 	-- Data
 	doc = "Lua documentation: %s", -- URL
 	app = "Apply to '%s': %s", -- Name, URL
 	noapp = "The requested team was not found. :(",
-	ateam = "Available teams → %s", -- List
+	ateam = "Type ',%s team_name'. Available teams → %s", -- List
 	nohelp = "Whisper me with ',%s' to get the command list.", -- "help"
 	about = "I'm a bot from the '%s' server ( %s ), maintained by %s. Shades Helpers is a group intended to help everyone, mostly about modules, lua, and technical stuff.", -- Name, URL, Name
 	nohelper = "No Shades Helpers online on Discord. :(",
@@ -38,14 +39,15 @@ return {
 	rewardscode = "Use the following code: %s",
 	notmapcrew = "You must be a Mapcrew in order to use this command.",
 	nomapcrew = "There aren't any Map Crew online. :(",
-	-- Extra
+	bgcolor = "The background color of Transformice is: %s",
 	outfit = "%s's outfit: %s", -- Name, URL
+	-- Extra
 	onteam = "Online%s members: %s", -- Name of the team (prefixed with a space), List
 	noteam = "No%s online members.", -- Name of the team (prefixed with a space)
 	spam = "Wow, %s; Hold on, cowboy! Don't spam me with commands.", -- Name
 	checktitle = "%d %s for «%s»",
 	notitle = "You already have all the titles from stats! :O",
-	interr = "An internal error occurred on Club-Mice. Try again later.",
+	interr = "An internal error occurred on CheeseForMice. Try again later.",
 	-- Fields
 	cheese = "cheese",
 	first = "first%s",
@@ -56,5 +58,13 @@ return {
 	-- Others
 	maintenance = "This command is under maintenance.",
 	tryCommand = "Did you mean: %s ?",
-	cooldown = "You have to wait a few minutes before using this command again."
+	cooldown = "You have to wait a few minutes before using this command again.",
+	-- Fast Reply
+	upperlua = "Lua*, it's not an acronym. Noob %s!",
+	-- Modules bbcode
+	bbcodeCommunity = "Commu",
+	bbcodeLevel = "Level",
+	bbcodeHoster = "Hoster",
+	bbcodeOfficial = "official",
+	bbcodeSemiOfficial = "semi-official"
 }
